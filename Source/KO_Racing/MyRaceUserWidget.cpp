f40acb6b56cf55d52e507edfe087e38428fdf2fa
yosmont <yann.osmont@epitech.eu>
2022-12-25 22:58:55 +0100

    working RaceHUD

--- Source/KO_Racing/MyRaceUserWidget.cpp
+++ Source/KO_Racing/MyRaceUserWidget.cpp
@@ -3,3 +3,57 @@
 
 #include "MyRaceUserWidget.h"
 
+void UMyRaceUserWidget::ChangeRankValue(float value)
+{
+	_rankTxt->ChangeValue(value);
+}
+
+void UMyRaceUserWidget::ChangeLapValue(float value)
+{
+	_lapTxt->ChangeValue(value);
+}
+
+void UMyRaceUserWidget::ChangeSpeedValue(float value)
+{
+	_speedTxt->ChangeValue(value);
+}
+
+void UMyRaceUserWidget::StartTimer()
+{
+	_timer = 0;
+	_timerIsStarted = true;
+}
+
+void UMyRaceUserWidget::StopTimer()
+{
+	_timerIsStarted = false;
+}
+
+void UMyRaceUserWidget::UpdateTimer(float deltaTime)
+{
+	if (_timerIsStarted) {
+		_timer += deltaTime;
+		_timerTxt->SetText(FText::FromString(FString(GenTimerString(_timer).c_str())));
+	}
+}
+
+float UMyRaceUserWidget::GetTimer()
+{
+	return _timer;
+}
+
+std::string UMyRaceUserWidget::GenTimerString(float time)
+{
+	int minute = time / 60;
+	int second = time - (minute * 60);
+	int ms = (time - second - (minute * 60)) * 1000;
+	std::string txt = "00:00.000";
+	txt[0] = '0' + (minute / 10);
+	txt[1] = '0' + (minute - ((minute / 10) * 10));
+	txt[3] = '0' + (second / 10);
+	txt[4] = '0' + (second - ((second / 10) * 10));
+	txt[6] = '0' + (ms / 100);
+	txt[7] = '0' + ((ms - ((ms / 100) * 100)) / 10);
+	txt[8] = '0' + (ms - ((ms / 10) * 10));
+	return txt;
+}
--- Source/KO_Racing/MyRaceUserWidget.h
+++ Source/KO_Racing/MyRaceUserWidget.h
@@ -5,6 +5,10 @@
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 
+#include "MyTextBlockColorChanger.h"
+#include "Components/TextBlock.h"
+#include <string>
+
 #include "MyRaceUserWidget.generated.h"
 
 /**
@@ -15,4 +19,34 @@ class KO_RACING_API UMyRaceUserWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+    UFUNCTION(BlueprintCallable)
+        void ChangeRankValue(float value);
+    UFUNCTION(BlueprintCallable)
+        void ChangeLapValue(float value);
+    UFUNCTION(BlueprintCallable)
+        void ChangeSpeedValue(float value);
+    UFUNCTION(BlueprintCallable)
+        void StartTimer();
+    UFUNCTION(BlueprintCallable)
+        void StopTimer();
+    UFUNCTION(BlueprintCallable)
+        void UpdateTimer(float deltaTime);
+    UFUNCTION(BlueprintCallable)
+        float GetTimer();
+
+    // All my variables
+    UPROPERTY(Category = TextColorChanger, EditAnywhere, BlueprintReadWrite)
+        UMyTextBlockColorChanger *_rankTxt;
+    UPROPERTY(Category = TextColorChanger, EditAnywhere, BlueprintReadWrite)
+        UMyTextBlockColorChanger *_lapTxt;
+    UPROPERTY(Category = TextColorChanger, EditAnywhere, BlueprintReadWrite)
+        UMyTextBlockColorChanger *_speedTxt;
+    UPROPERTY(Category = Text, EditAnywhere, BlueprintReadWrite)
+        UTextBlock *_timerTxt;
+protected:
+    static std::string GenTimerString(float time);
+
+    float _timer;
+    bool _timerIsStarted;
 };
