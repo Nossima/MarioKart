6bea091ff3cf3592243e797b120db9fe66f72e67
yosmont <yann.osmont@epitech.eu>
2023-01-06 21:37:05 +0100

    add background to the main menu

---
+++ Source/KO_Racing/MySlidingButton.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Button.h"
+#include "MySlidingButton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KO_RACING_API UMySlidingButton : public UButton
+{
+	GENERATED_BODY()
+	
+};
