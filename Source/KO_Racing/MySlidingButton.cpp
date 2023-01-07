800b3699b5773be549f2387059faaa68a4d45935
yosmont <yann.osmont@epitech.eu>
2023-01-07 18:36:45 +0100

    adding slidding button, linking MainMenu to a test map, Quit button on MainMenu to quit the game (it work, don't test it, it will close the editor too)

--- Source/KO_Racing/MySlidingButton.cpp
+++ Source/KO_Racing/MySlidingButton.cpp
@@ -3,3 +3,75 @@
 
 #include "MySlidingButton.h"
 
+void UMySlidingButton::Init()
+{
+	_startingX = RenderTransform.Translation.X;
+	_slidedX = _startingX - _maxOffset;
+	_timer = 0;
+}
+
+void UMySlidingButton::Tick(float deltaTime)
+{
+	Move(deltaTime);
+}
+
+void UMySlidingButton::Move(float deltaTime)
+{
+	if (IsHovered()) {
+		if (!_wasHovered) {
+			_timer = 0;
+			_wasHovered = true;
+		}
+		_timer += deltaTime;
+		if (_timer < _timeToTransition) {
+			FVector2D btnTxtPos = _btnTxt->RenderTransform.Translation;
+			FVector2D btnPos = RenderTransform.Translation;
+			float resX = (btnPos.X - _slidedX) * (_timer / _timeToTransition);
+			btnTxtPos.X += resX;
+			btnPos.X -= resX;
+			_btnTxt->SetRenderTranslation(btnTxtPos);
+			SetRenderTranslation(btnPos);
+		} else {
+			FVector2D btnTxtPos = _btnTxt->RenderTransform.Translation;
+			FVector2D btnPos = RenderTransform.Translation;
+			float resX = btnPos.X - _slidedX;
+			btnTxtPos.X += resX;
+			btnPos.X -= resX;
+			_btnTxt->SetRenderTranslation(btnTxtPos);
+			SetRenderTranslation(btnPos);
+		}
+	} else {
+		if (_wasHovered) {
+			_timer = 0;
+			_wasHovered = false;
+		}
+		_timer += deltaTime;
+		if (_timer < _timeToTransition) {
+			FVector2D btnTxtPos = _btnTxt->RenderTransform.Translation;
+			FVector2D btnPos = RenderTransform.Translation;
+			float resX = (_startingX - btnPos.X) * (_timer / _timeToTransition);
+			btnTxtPos.X -= resX;
+			btnPos.X += resX;
+			_btnTxt->SetRenderTranslation(btnTxtPos);
+			SetRenderTranslation(btnPos);
+		} else {
+			FVector2D btnTxtPos = _btnTxt->RenderTransform.Translation;
+			FVector2D btnPos = RenderTransform.Translation;
+			float resX = _startingX - btnPos.X;
+			btnTxtPos.X -= resX;
+			btnPos.X += resX;
+			_btnTxt->SetRenderTranslation(btnTxtPos);
+			SetRenderTranslation(btnPos);
+		}
+	}
+}
+
+void UMySlidingButton::LoadNextLevel()
+{
+	GetWorld()->ServerTravel(_nextLevelPath);
+}
+
+void UMySlidingButton::QuitGame()
+{
+	FGenericPlatformMisc::RequestExit(false);
+}
--- Source/KO_Racing/MySlidingButton.h
+++ Source/KO_Racing/MySlidingButton.h
@@ -4,6 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "Components/Button.h"
+
+#include "Engine/Texture.h"
+#include "Components/TextBlock.h"
+#include "Components/Image.h"
+
 #include "MySlidingButton.generated.h"
 
 /**
@@ -14,4 +19,31 @@ class KO_RACING_API UMySlidingButton : public UButton
 {
 	GENERATED_BODY()
 	
+public:
+	UFUNCTION(BlueprintCallable)
+		void Init();
+	UFUNCTION(BlueprintCallable)
+		void Tick(float deltaTime);
+	UFUNCTION(BlueprintCallable)
+		void Move(float deltaTime);
+	UFUNCTION(BlueprintCallable)
+		void LoadNextLevel();
+	UFUNCTION(BlueprintCallable)
+		void QuitGame();
+
+	UPROPERTY(Category = Sliding, EditAnywhere, BlueprintReadWrite)
+		float _timeToTransition;
+	UPROPERTY(Category = Sliding, EditAnywhere, BlueprintReadWrite)
+		float _speed;
+	UPROPERTY(Category = Sliding, EditAnywhere, BlueprintReadWrite)
+		float _maxOffset;
+	UPROPERTY(Category = BtnTxt, EditAnywhere, BlueprintReadWrite)
+		UTextBlock* _btnTxt;
+	UPROPERTY(Category = OnClickOptional, EditAnywhere, BlueprintReadWrite)
+		FString _nextLevelPath;
+protected:
+	float _startingX;
+	float _slidedX;
+	float _timer;
+	bool _wasHovered;
 };
